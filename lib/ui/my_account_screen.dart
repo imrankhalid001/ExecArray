81ff86414116b6c1c8bb87a06671012c60634698
imran khalid <imrankhalid1138903@gmail.com>
2024-08-06 23:16:05 +0500

    minor changes
    minor changes

--- lib/ui/my_account_screen.dart
+++ lib/ui/my_account_screen.dart
@@ -7,12 +7,12 @@ class MyAccountScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Account', style: TextStyle(color: Colors.white),),
+        title: const Text('My Account', style: TextStyle(color: Colors.white),),
         backgroundColor: Theme.of(context).primaryColor,
         centerTitle: true,
         actions: [
           IconButton(
-            icon: Icon(Icons.settings, color: Colors.white,),
+            icon: const Icon(Icons.settings, color: Colors.white,),
             onPressed: () {
               // Handle settings button press
             },
@@ -31,62 +31,59 @@ class MyAccountScreen extends StatelessWidget {
   }
 
    Widget _buildProfileSection() {
-    return Container(
-      // Background color from the image
-      child: Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.none, // Ensures children can overflow the stack
-            children: <Widget>[
-              Image.asset(
-                'assets/account_top.png',
-                height: 150,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-              const Positioned(
-                left: 0,
-                right: 0,
-                top: 100, // Adjust this value to position the CircleAvatar
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Card(
-                      elevation: 5,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        radius: 50,
-                        backgroundImage: AssetImage('assets/user_profile.png'),
-                      ),
+    return Column(
+      children: [
+        Stack(
+          clipBehavior: Clip.none, // Ensures children can overflow the stack
+          children: <Widget>[
+            Image.asset(
+              'assets/account_top.png',
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const Positioned(
+              left: 0,
+              right: 0,
+              top: 100, // Adjust this value to position the CircleAvatar
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    elevation: 5,
+                    shape: CircleBorder(),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage('assets/user_profile.png'),
                     ),
-                    SizedBox(height: 16),
-                    Text(
-                      'Hi,\nImran Khalid',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Hi,\nImran Khalid',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
 
   Widget _buildMenuSection(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       child: Column(
         children: [
-          SizedBox(height: 150,),
-             Divider(),
+        const  SizedBox(height: 150,),
+           const  Divider(),
           _buildMenuItem(
             icon: Icons.shopping_cart_outlined,
             title: 'My Orders',
@@ -94,7 +91,7 @@ class MyAccountScreen extends StatelessWidget {
               // Handle My Orders tap
             },
           ),
-          Divider(),
+        const  Divider(),
           _buildMenuItem(
             icon: Icons.sell_outlined,
             title: 'Sell With Us',
@@ -102,7 +99,7 @@ class MyAccountScreen extends StatelessWidget {
               // Handle Sell With Us tap
             },
           ),
-            Divider(),
+        const    Divider(),
           _buildMenuItem(
             icon: Icons.person_2_outlined,
             title: 'My Details',
@@ -111,7 +108,7 @@ class MyAccountScreen extends StatelessWidget {
             },
           ),
 
-            Divider(),
+          const  Divider(),
           _buildMenuItem(
             icon: Icons.key,
             title: 'Change Password',
@@ -120,7 +117,7 @@ class MyAccountScreen extends StatelessWidget {
             },
           ),
 
-            Divider(),
+          const  Divider(),
           _buildMenuItem(
             icon: Icons.payment,
             title: 'Payment methods',
@@ -129,7 +126,7 @@ class MyAccountScreen extends StatelessWidget {
             },
           ),
 
-                Divider(),
+           const  Divider(),
           _buildMenuItem(
             icon: Icons.home_outlined,
             title: 'Delivery Address',
@@ -138,7 +135,7 @@ class MyAccountScreen extends StatelessWidget {
             },
           ),
 
-          Divider(),
+        const  Divider(),
           _buildMenuItem(
             icon: Icons.support_agent,
             title: 'Support Center',
@@ -146,7 +143,7 @@ class MyAccountScreen extends StatelessWidget {
               // Handle Support Center tap
             },
           ),
-          Divider(),
+         const Divider(),
           _buildMenuItem(
             icon: Icons.info_outline,
             title: 'About Us',
@@ -154,7 +151,7 @@ class MyAccountScreen extends StatelessWidget {
               // Handle About Us tap
             },
           ),
-          Divider(),
+        const  Divider(),
         ],
       ),
     );
@@ -163,7 +160,7 @@ class MyAccountScreen extends StatelessWidget {
   Widget _buildMenuItem({required IconData icon, required String title, required VoidCallback onTap}) {
     return ListTile(
       leading: Icon(icon),
-      trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.grey),
       title: Text(title),
       onTap: onTap,
     );

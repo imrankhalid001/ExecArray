f6c33681fcbe69328b2e09023b0cf8881a6257ab
imran khalid <imrankhalid1138903@gmail.com>
2024-08-11 22:28:58 +0500

    minor change
    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: RoutesName.splash,
       onGenerateRoute: Routes.generateRoute,
-      home:  HomePage(),
+      home:  HomePage(), 
     );
   }
 }
